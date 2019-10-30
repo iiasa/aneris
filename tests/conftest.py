39aebfda93adad0c3f30ce24059973b47a0093f2
Matthew Gidden <matthew.gidden@gmail.com>
2019-10-30 12:34:37 +0100

    still referenced runslow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 
+# TODO: utilize this for regression or take it out completely
 # def pytest_addoption(parser):
 #     parser.addoption("--runslow", action="store_true",
 #                      help="run slow tests")
--- tests/test_regression.py
+++ tests/test_regression.py
@@ -6,11 +6,13 @@ from pandas.util.testing import assert_frame_equal
 
 from aneris import cli
 
-# decorator for slow-running tests
-slow = pytest.mark.skipif(
-    not pytest.config.getoption("--runslow"),
-    reason="need --runslow option to run"
-)
+
+# TODO: utilize this for regression or take it out completely
+# # decorator for slow-running tests
+# slow = pytest.mark.skipif(
+#     not pytest.config.getoption("--runslow"),
+#     reason="need --runslow option to run"
+# )
 
 
 # This is a class that runs all tests through the harmonize CLI Note that it
@@ -62,6 +64,7 @@ class TestHarmonizeRegression():
         checkf = 'test_basic_run.xlsx'
         self._run(prefix, inf, checkf, hist='history.xls', reg='regions.csv')
 
+    # TODO: utilize this for regression or take it out completely
     # there were a number of these tests. I now no longer know where the regression files exist.
     # the best option here is to use a token on CI to test this by downloading an existing file.
     #
