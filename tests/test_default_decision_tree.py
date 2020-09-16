139f68feaa12d0f6b84c6de9e082c0f26d81df1d
Jonas Hoersch <jonas.hoersch@climateanalytics.org>
2020-09-16 10:39:27 +0200

    tests: Add test with custom method_choice

--- tests/test_default_decision_tree.py
+++ tests/test_default_decision_tree.py
@@ -18,6 +18,11 @@ def index1():
     return make_index(1)
 
 
+@pytest.fixture
+def index1_co2():
+    return make_index(1, gas='CO2')
+
+
 def test_hist_zero(index1):
     hist = pd.DataFrame({'2015': [0]}, index1)
     df = pd.DataFrame({'2015': [1.]}, index1)
@@ -136,3 +141,32 @@ def test_branch6(index1):
 
     exp = pd.Series(['reduce_offset_2150_cov'], index1, name='methods')
     pdt.assert_series_equal(exp, obs, check_names=False)
+
+
+def test_custom_method_choice(index1, index1_co2):
+    def method_choice(
+        row, ratio_method, offset_method, luc_method, luc_cov_threshold
+    ):
+        return 'budget' if row.gas == 'CO2' else ratio_method
+
+    # CH4
+    hist_ch4 = pd.DataFrame({'2015': [1.]}, index1)
+    df_ch4 = pd.DataFrame({'2015': [1.]}, index1)
+
+    obs_ch4, _ = harmonize.default_methods(
+        hist_ch4, df_ch4, '2015', method_choice=method_choice
+    )
+
+    exp_ch4 = pd.Series(['reduce_ratio_2080'], index1, name='methods')
+    pdt.assert_series_equal(exp_ch4, obs_ch4, check_names=False)
+
+    # CO2
+    hist_co2 = pd.DataFrame({'2015': [1.]}, index1_co2)
+    df_co2 = pd.DataFrame({'2015': [1.]}, index1_co2)
+
+    obs_co2, _ = harmonize.default_methods(
+        hist_co2, df_co2, '2015', method_choice=method_choice
+    )
+
+    exp_co2 = pd.Series(['budget'], index1_co2, name='methods')
+    pdt.assert_series_equal(exp_co2, obs_co2, check_names=False)
